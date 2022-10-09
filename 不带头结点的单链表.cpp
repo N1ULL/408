a95385e8192509827413cf186bdcb7f621b0362f
N1ULL <34934155+N1ULL@users.noreply.github.com>
2022-10-09 22:51:57 +0800

    Add files via upload

---
+++ 不带头结点的单链表.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct LNode //定义单链表节点类型
+{
+    int data;//每个节点存放一个数据元素
+    struct LNode *next;//指针指向下一个节点
+}LNode,*LinkList;
+
+bool InitList(LinkList &L)
+{
+    L = NULL;//
+    return true;
+}
+
+void test()
+{
+    LinkList L;//声明一个指向单链表的指针
+    //初始化一个空表
+    InitList(L);
+    //...后续代码...
+}
